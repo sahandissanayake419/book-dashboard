4389345ca8b1e767cfdc11d0177ff8ad33104aa0
sahandissanayake419 <sahandissanayake419@gmail.com>
2025-05-23 00:29:40 +0530

    Initial commit

---
+++ src/app/components/book-table/book-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-table',
+  imports: [],
+  templateUrl: './book-table.component.html',
+  styleUrl: './book-table.component.css'
+})
+export class BookTableComponent {
+
+}
